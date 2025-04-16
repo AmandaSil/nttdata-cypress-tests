5afc1d61b9fdd23d3da112e001e89b1e9939473c
AmandaSil <uaipaaty@gmail.com>
2025-04-16 17:42:43 -0300

    Realizando alguns ajustes

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,28 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://front.serverest.dev",
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 15000,
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: false,
+    video: false,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        if (browser.name === "chrome") {
+          
+          launchOptions.args.push("--ignore-certificate-errors");
+        }
+        return launchOptions;
+      });
     },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+const app = window.top;
+if (!app.document.head.querySelector("[data-hide-command-log-request]")){
+    const style = app.document.createElement("style");
+    style.innerHTML = `.command-name-request, .command-name-xhr,
+    .command-name-uncaught-expection  { display: none};`;
+    style.setAttribute("data-hide-command-log-request", "");
+    app.document.head.appendChild(style);
+}
+
+Cypress.on("uncaught:exception", () => {
+    return false;
+})
